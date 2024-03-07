19578e14ec02d127ae33d4d74079dd137ba62a68
emilyaaleqsanyan <emilyaaleqsanyan@gmail.com>
2024-03-07 21:46:54 +0400

    edite CourseSchedule entity

--- src/main/java/am/itspace/kidsacademy/entity/CourseSchedule.java
+++ src/main/java/am/itspace/kidsacademy/entity/CourseSchedule.java
@@ -2,14 +2,17 @@ package am.itspace.kidsacademy.entity;
 
 import am.itspace.kidsacademy.entity.enums.Day;
 import jakarta.persistence.Table;
-import jakarta.persistence.Id;
 import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
-import java.util.List;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Enumerated;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Id;
+
+import java.util.List;
+
 import lombok.EqualsAndHashCode;
 import org.springframework.format.annotation.DateTimeFormat;
 import java.time.LocalTime;
@@ -33,7 +36,7 @@ public class CourseSchedule {
     @Enumerated(EnumType.STRING)
     private Day day;
 
-    @OneToMany
+    @OneToMany(mappedBy = "courseSchedule", cascade = CascadeType.ALL)
     private List<Course> courses;
 
     @Override
